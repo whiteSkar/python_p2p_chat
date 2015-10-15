25c7093b909875ff5894a309e95e0a446383f8fd
whiteSkar <sypower07@hotmail.com>
2015-10-14 21:30:24 -0700

    message window for client. pressing x properly closes the app

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import socket
 
 
@@ -8,6 +9,8 @@ PORT = 50007	# same as server
 
 class Client(tk.Frame):
     def __init__(self, master=None):
+        master.protocol("WM_DELETE_WINDOW", self.close_app)
+        
         tk.Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
@@ -16,24 +19,31 @@ class Client(tk.Frame):
         self.s.connect((HOST, PORT))
 
     def createWidgets(self):
+        self.msg_window = tk.Text(self, height=10, width=80)
+        self.msg_window.pack(side="top")
+        
+        self.msg_entry = tk.Entry(self)
+        self.msg_entry.pack(side="left")
+
         self.send_button = tk.Button(self)
         self.send_button["text"] = "Send"
         self.send_button["command"] = self.send_msg
         self.send_button.pack(side="top")
 
-        self.quit_button = tk.Button(self, text="QUIT", fg="red", command=self.close_app)
-        self.quit_button.pack(side="bottom")
-
     def send_msg(self):
-        self.s.sendall(('message from client').encode())
+        msg = self.msg_entry.get()
+        self.msg_entry.delete(0, tk.END)
+        self.s.sendall((msg).encode())
         
     def close_app(self):
-        self.s.close()
-        root.destroy()
+        #if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.s.close()
+            root.destroy()
 
 
 root = tk.Tk()
 client = Client(master=root)
+
 client.mainloop()
 
 

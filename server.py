aa53fcfb00ab79498132a3c863e0cca3b3739ce6
whiteSkar <sypower07@hotmail.com>
2015-11-05 17:45:08 -0800

    classified(not the dictionary meaning) server.py

--- server.py
+++ server.py
@@ -1,28 +1,35 @@
 import threading
 import socket
 
-def client_handler(conn):
-    while True:
-        data = conn.recv(1024) # bufsize should be a small power of 2 like 4096
-        if not data:
-            break
-
-        conn.sendall(data)
-    
 
 HOST = ''
 PORT = 50007
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
 
-while True:
-    s.listen(1)	# maximum # of queued connections
-    conn, addr = s.accept()	# conn is the new socket. addr is the client addr
-    print("Connected by", addr) # why can't i use %s?
+class Server():
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((HOST, PORT))
     
-    th = threading.Thread(target=client_handler, kwargs={'conn': conn})
-    th.start()
+    def client_handler(self, conn):
+        while True:
+            data = conn.recv(1024) # bufsize should be a small power of 2 like 4096
+            if not data:
+                break
+
+            conn.sendall(data)
+        
+    def run(self):
+        while True:
+            self.s.listen(1)	# maximum # of queued connections
+            conn, addr = self.s.accept()	# conn is the new socket. addr is the client addr
+            print("Connected by", addr) # why can't i use %s?
+            
+            th = threading.Thread(target=self.client_handler, kwargs={'conn': conn})
+            th.start()
+
+        conn.close()
 
 
-conn.close()
+server = Server()
+server.run()
